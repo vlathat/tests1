3a76776c56b892ac4662868731f4da19c2c01dfe
latha <latha@192.168.0.14>
2015-09-16 13:53:23 -0500

    l

---
+++ 1/src/test/java/jar/HelloMavenTest.java
@@ -0,0 +1,25 @@
+package jar;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class HelloMavenTest {
+	
+	
+
+	
+	@Test
+	public void login ()
+	{ 
+		System.out.println("Hello world test maven");
+		WebDriver Driver = new FirefoxDriver();
+		Driver.navigate();
+				
+	}
+	
+
+}
+
+
+
